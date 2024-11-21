c0cabd3863ab257175801e491c74216d9fc751ab
Maguette NDIAYE <maguettendiaye1@esp.sn>
2024-11-21 16:41:10 +0000

    first commit

--- UsingCollectionsDemo/Employé.cs
+++ UsingCollectionsDemo/Employé.cs
@@ -1,13 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections;
 
 namespace UsageCollections
 {
-    public class Employé
+    public class Etudiant
     {
         public string Nom { get; set; }
-        public int Age { get; set; }
+        public double NoteCC { get; set; } // Note de Contrôle Continu
+        public double NoteDevoir { get; set; } // Note de Devoir
+
+        /// <summary>
+        /// Calcul de la moyenne pondérée (33% pour NoteCC et 67% pour NoteDevoir)
+        /// </summary>
+        /// <returns>Moyenne pondérée</returns>
+        public double CalculerMoyenne()
+        {
+            return (NoteCC * 0.33) + (NoteDevoir * 0.67);
+        }
+
+        public override string ToString()
+        {
+            return $"Nom: {Nom}, Note CC: {NoteCC}, Note Devoir: {NoteDevoir}, Moyenne: {CalculerMoyenne():F2}";
+        }
     }
 }
--- UsingCollectionsDemo/Program.cs
+++ UsingCollectionsDemo/Program.cs
@@ -1,38 +1,132 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
+using System;
+using UsageCollections;
 
-namespace UsageCollections
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        SortedList lstEtudiants = new SortedList();
+
+        while (true)
         {
-            SortedList lstEmployé = new SortedList();
-            lstEmployé.Add("Raci" +
-                "ne", new Employé { Nom = "Racine", 
-                Age= 32 });
-            Employé layssa = new Employé()
-            {
-                Nom = "layssa",
-                Age = 25
-            };
-            lstEmployé.Add(layssa.Nom, layssa);
-            Employé unEmployé = (Employé)lstEmployé["Racine"];
+            Console.WriteLine("\nMenu :");
+            Console.WriteLine("1. Ajouter un étudiant");
+            Console.WriteLine("2. Afficher les détails d'un étudiant");
+            Console.WriteLine("3. Afficher tous les étudiants");
+            Console.WriteLine("4. Quitter le programme");
+            Console.Write("Votre choix : ");
+            string choix = Console.ReadLine().ToLower();
 
-            if (unEmployé != null)
+            if (choix == "q" || choix == "quitter")
             {
-                Console.WriteLine($"Nom: {unEmployé.Nom}, Age:" +
-                    $" {unEmployé.Age.ToString()}");
+                Console.WriteLine("Programme terminé.");
+                return;
             }
 
-            Console.ReadLine();
-
-            foreach (DictionaryEntry employé in lstEmployé)
+            switch (choix)
             {
-                Employé autreEmployé = (Employé)employé.Value;
-                Console.WriteLine($"Nom: {autreEmployé.Nom}, Age: {autreEmployé.Age.ToString()}");
+                case "1":
+                    AjouterEtudiant(lstEtudiants);
+                    break;
+                case "2":
+                    AfficherUnEtudiant(lstEtudiants);
+                    break;
+                case "3":
+                    AfficherTousLesEtudiants(lstEtudiants);
+                    break;
+                case "4":
+                    Console.WriteLine("Programme terminé.");
+                    return;
+                default:
+                    Console.WriteLine("Choix invalide. Veuillez réessayer.");
+                    break;
             }
-            Console.ReadLine();
+        }
+    }
+
+    /// <summary>
+    /// Ajoute un étudiant à la collection
+    /// </summary>
+    /// <param name="lstEtudiants">Liste des étudiants</param>
+    static void AjouterEtudiant(SortedList lstEtudiants)
+    {
+        Console.Write("Nom de l'étudiant : ");
+        string nom = Console.ReadLine();
+
+        Console.Write("Note de Contrôle Continu (sur 20) : ");
+        double noteCC = LireNote();
+
+        Console.Write("Note de Devoir (sur 20) : ");
+        double noteDevoir = LireNote();
+
+        Etudiant etudiant = new Etudiant
+        {
+            Nom = nom,
+            NoteCC = noteCC,
+            NoteDevoir = noteDevoir
+        };
+
+        lstEtudiants.Add(nom, etudiant);
+        Console.WriteLine("Étudiant ajouté avec succès !");
+    }
+
+    /// <summary>
+    /// Affiche les détails d'un étudiant spécifique
+    /// </summary>
+    /// <param name="lstEtudiants">Liste des étudiants</param>
+    static void AfficherUnEtudiant(SortedList lstEtudiants)
+    {
+        Console.Write("Entrez le nom de l'étudiant à rechercher : ");
+        string nom = Console.ReadLine();
+
+        if (lstEtudiants.Contains(nom))
+        {
+            Etudiant etudiant = (Etudiant)lstEtudiants[nom];
+            Console.WriteLine(etudiant.ToString());
+        }
+        else
+        {
+            Console.WriteLine("Aucun étudiant trouvé avec ce nom.");
+        }
+    }
+
+    /// <summary>
+    /// Affiche les détails de tous les étudiants
+    /// </summary>
+    /// <param name="lstEtudiants">Liste des étudiants</param>
+    static void AfficherTousLesEtudiants(SortedList lstEtudiants)
+    {
+        if (lstEtudiants.Count == 0)
+        {
+            Console.WriteLine("Aucun étudiant dans la liste.");
+            return;
+        }
+
+        Console.WriteLine("\nListe des étudiants :");
+        foreach (DictionaryEntry entry in lstEtudiants)
+        {
+            Etudiant etudiant = (Etudiant)entry.Value;
+            Console.WriteLine(etudiant.ToString());
+        }
+    }
+
+    /// <summary>
+    /// Lecture sécurisée d'une note avec validation
+    /// </summary>
+    /// <returns>Note valide entre 0 et 20</returns>
+    static double LireNote()
+    {
+        while (true)
+        {
+            if (double.TryParse(Console.ReadLine(), out double note) && note >= 0 && note <= 20)
+            {
+                return note;
+            }
+            else
+            {
+                Console.Write("Veuillez entrer une note valide (entre 0 et 20) : ");
+            }
         }
     }
 }
