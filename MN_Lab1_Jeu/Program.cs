053b3d4e403c871c7ef3ec2dfc39a82e5ab199a4
Maguette NDIAYE <maguettendiaye1@esp.sn>
2024-11-21 17:35:09 +0000

    Ajoutez des fichiers projet.

---
+++ MN_Lab1_Jeu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MN_Lab1_Jeu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
